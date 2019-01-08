5a14b3f2c97e6e07db5b9b13cef7559ef0b8c376
Kristian Borysiak <kborysiak@ucvts.tec.nj.us>
2019-01-08 13:21:17 -0500

    comma delimitation done, issues arose, need to figure out issues

--- calculator.js
+++ calculator.js
@@ -1,14 +1,15 @@
 function insert(num){
   document.calc.display.value = document.calc.display.value + num;
 }
+
 function clearCalc(){
   document.calc.display.value="";
-
 }
 function equal(){
   let calcAnswer= document.calc.display.value;
+
   if(calcAnswer){
-    document.calc.display.value=eval(calcAnswer);
+    document.calc.display.value=eval(calcAnswer).toLocaleString("en");
   }
   if(document.calc.display.value === "Infinity") {
     document.calc.display.value = "ERROR";
